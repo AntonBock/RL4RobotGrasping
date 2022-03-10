bd663c04cdf6b445f7b8636cbc94324c71dd98ff
marius139 <marius139@gmail.com>
2022-03-10 13:36:19 +0100

    ADD: Scoop grasp setup

--- env/tasks/franka_cabinet.py
+++ env/tasks/franka_cabinet.py
@@ -298,7 +298,7 @@ class FrankaCabinet(VecTask):
                             prop_state_pose.p.x = randrange_float(0.40, 0.80, self.prop_spacing) # returns 2.4
                             # propz, propy = 0, p
                             prop_state_pose.p.y = randrange_float(-0.50, 0.50, self.prop_spacing)
-                            prop_state_pose.p.z = 0.05
+                            prop_state_pose.p.z = 0.026
                             yaw = random.uniform(0.00000, 6.28318)
             
 
@@ -407,8 +407,11 @@ class FrankaCabinet(VecTask):
         _force_vec = self.gym.acquire_net_contact_force_tensor(self.sim)
         force_vec = gymtorch.wrap_tensor(_force_vec)
         force_vec = force_vec.view(-1, 11, 3)
-        force_vec_left = force_vec.select(1, 8).cpu().numpy()
-        force_vec_right = force_vec.select(1, 9).cpu().numpy()
+        force_vec_left = force_vec.select(1, 8)
+        force_vec_right = force_vec.select(1, 9)
+
+        force_vec_left = force_vec_left.cpu().numpy()
+        force_vec_right = force_vec_right.cpu().numpy()
         #print(f"FORCES:{force_vec_left}")
         
         right_forces = []
@@ -436,9 +439,11 @@ class FrankaCabinet(VecTask):
             else:
                 force_tens[i] = 0
                 
+        contacts = self.gym.get_env_rigid_contacts(self.envs[0])
+        print(contacts)
 
         self.rew_buf[:], self.reset_buf[:] = compute_franka_reward(
-            self.reset_buf, self.progress_buf, self.actions, #self.cabinet_dof_pos,
+            self.reset_buf, self.progress_buf, self.actions,
             self.franka_grasp_pos, self.prop_grasp_pos, self.franka_grasp_rot, self.prop_grasp_rot,
             self.franka_lfinger_pos, self.franka_rfinger_pos,
             self.gripper_forward_axis, self.prop_inward_axis, self.gripper_up_axis, self.prop_up_axis,
@@ -523,7 +528,7 @@ class FrankaCabinet(VecTask):
                             prop_state_pose.p.x = randrange_float(0.40, 0.80, self.prop_spacing) # returns 2.4
                             # propz, propy = 0, p
                             prop_state_pose.p.y = randrange_float(-0.50, 0.50, self.prop_spacing)
-                            prop_state_pose.p.z = 0.05
+                            prop_state_pose.p.z = 0.026
                             
                             roll = 0
                             pitch = 0
@@ -672,27 +677,23 @@ def compute_franka_reward(
     rfinger_reward = torch.where(d <= 0.05, rfinger_reward * 2, rfinger_reward) * finger_dist_reward_scale
 
     action_penalty = torch.sum(actions ** 2, dim=-1) * action_penalty_scale
-
-    # How high the box has been lifted
-    height_reward = torch.where(prop_grasp_pos[:, 2]>10, 5, 0) * height_reward_scale  # drawer_top_joint
     
     finger_dist = torch.norm(franka_lfinger_pos - franka_rfinger_pos, p=2, dim=-1)
-   
+    finger_penalty = torch.where(finger_dist<0.03, 0.1, 0.0)
 
-    close_reward = torch.where(d <= 0.3, 0.05, 0.0)
-
+    close_reward = torch.where(d <= 0.3, 1.0, 0.0)
     dist_reward = torch.where(d <= 0.06, 10, 0)
-    height_reward = torch.where(prop_grasp_pos[:, 2]>4, 500, 0)
+    #height_reward = torch.where(prop_grasp_pos[:, 2]>4, 100, 0)
+    height_reward = torch.where(prop_grasp_pos[:, 2]>0.07, 10000, 0)
         
-    
-    time_penalty = 0.1
+    time_penalty = 0.05
 
-    grip_reward = grip_forces * 100
+    grip_reward = grip_forces * 10
 
-    rewards = dist_reward + close_reward + grip_reward - time_penalty
+    rewards = dist_reward + close_reward + height_reward + grip_reward - time_penalty - finger_penalty
 
     #reset_buf = torch.ones_like(reset_buf) if height_reward > 100 else reset_buf
-    #reset_buf = torch.where(d <= 0.06, torch.ones_like(reset_buf), reset_buf)
+    reset_buf = torch.where(prop_grasp_pos[:, 2]>0.07, torch.ones_like(reset_buf), reset_buf)
     reset_buf = torch.where(progress_buf >= max_episode_length - 1, torch.ones_like(reset_buf), reset_buf)
 
     return rewards, reset_buf
