ef8c9c95255d9c7e769fbdb9ee25eb1ca4d9e292
Anton <aan17@student.aau.dj>
2022-03-25 12:01:25 +0100

    UPD: Ad hoc

--- env/tasks/franka_cabinet.py
+++ env/tasks/franka_cabinet.py
@@ -308,7 +308,7 @@ class FrankaCabinet(VecTask):
                 subt=SubTerrain(width=num_rows, length=num_cols, vertical_scale=vertical_scale, horizontal_scale=horizontal_scale)
 
 
-                heightfield = random_uniform_terrain(subt, min_height=-0.2, max_height=0.0, step=0.01, downsampled_scale=0.3).height_field_raw
+                heightfield = random_uniform_terrain(subt, min_height=-0.2, max_height=0.0, step=0.01, downsampled_scale=0.5).height_field_raw
 
                 # add the terrain as a triangle mesh
                 vertices, triangles = convert_heightfield_to_trimesh(heightfield, horizontal_scale=horizontal_scale, vertical_scale=vertical_scale, slope_threshold=1.5)
