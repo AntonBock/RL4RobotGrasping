50d8738b65f43c838decac6e54ffd30f41169632
marius139 <marius139@gmail.com>
2022-03-29 09:17:54 +0200

    ADD: CNN for cartpole

--- skrl_cartpole_cnn_test.py
+++ skrl_cartpole_cnn_test.py
@@ -12,7 +12,7 @@ from skrl.trainers.torch import SequentialTrainer
 from skrl.envs.torch import wrap_env
 from skrl.envs.torch import load_isaacgym_env_preview2, load_isaacgym_env_preview3
 
-
+tt = True #training time
 # Define the models (stochastic and deterministic models) for the agent using helper classes 
 # and programming with two approaches (layer by layer and torch.nn.Sequential class).
 # - Policy: takes as input the environment's observation/state and returns an action
@@ -23,58 +23,59 @@ class Policy(GaussianModel):
         super().__init__(observation_space, action_space, device, clip_actions,
                          clip_log_std, min_log_std, max_log_std)
 
-        self.net = nn.Sequential(nn.Conv2d(1, 32, kernel_size=8, stride=4),
+        self.net_img = nn.Sequential(nn.Conv2d(1, 4, kernel_size=8, stride=4),
                                  nn.ReLU(),
-                                 nn.Conv2d(32, 64, kernel_size=4, stride=2),
+                                 nn.Conv2d(4, 8, kernel_size=4, stride=2),
                                  nn.ReLU(),
-                                 nn.Conv2d(64, 64, kernel_size=3, stride=1),
+                                 nn.Conv2d(8, 8, kernel_size=4, stride=2),
                                  nn.ReLU(),
-                                 nn.Flatten(),
-                                 nn.Linear(1024, 512),
-                                 nn.ReLU(),
-                                 nn.Linear(512, 16),
-                                 nn.Tanh(),
-                                 nn.Linear(16, 64),
-                                 nn.Tanh(),
-                                 nn.Linear(64, 32),
-                                 nn.Tanh(),
-                                 nn.Linear(32, self.num_actions))
+                                 nn.Flatten())
+
+        self.net_fc = nn.Sequential(nn.Linear(36, 32),
+                                    nn.ReLU(),
+                                    nn.Linear(32, self.num_actions))
+        
         self.log_std_parameter = nn.Parameter(torch.zeros(self.num_actions))
 
     def compute(self, states, taken_actions):
-        # print(f"states: {states.shape}")
+        #print(f"states: {states.shape}")
+        #Split states into image and robot data, states --> [img, data]
+        img, data = torch.split(states, [64*64, 4], dim=1)
+
         # print(f"observation space: {self.observation_space.shape}")
         # view (samples, width * height * channels) -> (samples, width, height, channels) 
         # permute (samples, width, height, channels) -> (samples, channels, width, height) 
-        x = self.net(states.view(-1, 64, 64, 1).permute(0, 3, 1, 2))
-        return torch.tanh(x), self.log_std_parameter
+        x = self.net_img(img.view(-1, 64, 64, 1).permute(0, 3, 1, 2))
+        y = torch.cat((x, data), 1)
+        z = self.net_fc(y)
+        return torch.tanh(z), self.log_std_parameter
 
 class Value(DeterministicModel):
     def __init__(self, observation_space, action_space, device, clip_actions = False):
         super().__init__(observation_space, action_space, device, clip_actions)
 
-        self.net = nn.Sequential(nn.Conv2d(1, 32, kernel_size=8, stride=4),
+        self.net_img = nn.Sequential(nn.Conv2d(1, 4, kernel_size=8, stride=4),
                                  nn.ReLU(),
-                                 nn.Conv2d(32, 64, kernel_size=4, stride=2),
+                                 nn.Conv2d(4, 8, kernel_size=4, stride=2),
                                  nn.ReLU(),
-                                 nn.Conv2d(64, 64, kernel_size=3, stride=1),
+                                 nn.Conv2d(8, 8, kernel_size=4, stride=2),
                                  nn.ReLU(),
-                                 nn.Flatten(),
-                                 nn.Linear(1024, 512),
-                                 nn.ReLU(),
-                                 nn.Linear(512, 16),
-                                 nn.Tanh(),
-                                 nn.Linear(16, 64),
-                                 nn.Tanh(),
-                                 nn.Linear(64, 32),
-                                 nn.Tanh(),
-                                 nn.Linear(32, 1))
+                                 nn.Flatten())
+
+        self.net_fc = nn.Sequential(nn.Linear(36, 32),
+                                    nn.ReLU(),
+                                    nn.Linear(32, 1))
                                  
     
     def compute(self, states, taken_actions):
+        #Split states into image and robot data, split states (img, data)
+        img, data = torch.split(states, [64*64, 4], dim=1)
+
         # view (samples, width * height * channels) -> (samples, width, height, channels) 
         # permute (samples, width, height, channels) -> (samples, channels, width, height) 
-        return self.net(states.view(-1, 64, 64, 1).permute(0, 3, 1, 2))
+        x = self.net_img(img.view(-1, 64, 64, 1).permute(0, 3, 1, 2))
+        y = torch.cat((x, data), 1)
+        return self.net_fc(y)
 
 
 # Load and wrap the Isaac Gym environment.
@@ -94,39 +95,34 @@ memory = RandomMemory(memory_size=16, num_envs=env.num_envs, device=device)
 # PPO requires 2 models, visit its documentation for more details
 # https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#models-networks
 networks_ppo = {"policy": Policy(env.observation_space, env.action_space, device, clip_actions=True),
-                "value": Value(env.observation_space, env.action_space, device)}
+            "value": (Value(env.observation_space, env.action_space, device) if tt else None)}
+
 
 # Initialize the models' parameters (weights and biases) using a Gaussian distribution
-for network in networks_ppo.values():
-    network.init_parameters(method_name="normal_", mean=0.0, std=0.1)   
+if tt:
+    # Initialize the models' parameters (weights and biases) using a Gaussian distribution
+    for network in networks_ppo.values():
+        network.init_parameters(method_name="normal_", mean=0.0, std=0.1)     
+else:
+    networks_ppo["policy"].load("./runs/cnn_cart/checkpoints/1400_policy.pt") 
 
 
 # Configure and instantiate the agent.
 # Only modify some of the default configuration, visit its documentation to see all the options
 # https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#configuration-and-hyperparameters
 cfg_ppo = PPO_DEFAULT_CONFIG.copy()
-cfg_ppo["rollouts"] = 16
-cfg_ppo["learning_epochs"] = 2
-cfg_ppo["mini_batches"] = 8
-cfg_ppo["discount_factor"] = 0.99
-cfg_ppo["lambda"] = 0.95
-cfg_ppo["policy_learning_rate"] = 0.001
-cfg_ppo["value_learning_rate"] = 0.001
-cfg_ppo["random_timesteps"] = 0
 cfg_ppo["learning_starts"] = 0
-cfg_ppo["grad_norm_clip"] = 2
-cfg_ppo["ratio_clip"] = 0.2
-cfg_ppo["value_clip"] = 0.2
-cfg_ppo["clip_predicted_values"] = False
-cfg_ppo["entropy_loss_scale"] = 0.0
-cfg_ppo["value_loss_scale"] = 2
-cfg_ppo["kl_threshold"] = 0
-# logging to TensorBoard and write checkpoints each 10000 timesteps
-cfg_ppo["experiment"]["write_interval"] = 50
-cfg_ppo["experiment"]["checkpoint_interval"] = 500
+cfg_ppo["random_timesteps"] = 0
+cfg_ppo["rollouts"] = 16
+cfg_ppo["learning_epochs"] = 8
+cfg_ppo["grad_norm_clip"] = 2.0
+cfg_ppo["value_loss_scale"] = 2.0
+# logging to TensorBoard and write checkpoints each 16 and 1000 timesteps respectively
+cfg_ppo["experiment"]["write_interval"] = 16
+cfg_ppo["experiment"]["checkpoint_interval"] = 200
 
-agent = PPO(networks=networks_ppo,
-            memory=memory, 
+agent = PPO(models=networks_ppo,
+            memory=(memory if tt else None), 
             cfg=cfg_ppo, 
             observation_space=env.observation_space, 
             action_space=env.action_space,
@@ -138,4 +134,7 @@ cfg_trainer = {"timesteps": 100000, "progress_interval": 200}
 trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
 # start training
-trainer.train()
+if tt:
+    trainer.train()
+else:
+    trainer.eval()
